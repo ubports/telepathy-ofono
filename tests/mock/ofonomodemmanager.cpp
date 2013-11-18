895518c90d3d4c6051799de158879a283008e142
Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
2013-11-18 12:03:00 -0200

    Add mock implementation of ofonomodemmanager.

---
+++ tests/mock/ofonomodemmanager.cpp
@@ -0,0 +1,24 @@
+#include "ofonomodemmanager.h"
+
+// mock implementation of ofono-qt
+OfonoModemManager::OfonoModemManager(QObject *parent)
+: QObject(parent)
+{
+}
+
+OfonoModemManager::~OfonoModemManager()
+{
+}
+
+QStringList OfonoModemManager::modems() const
+{
+    return QStringList() << "/mock";
+}
+
+void OfonoModemManager::onModemAdded(const QDBusObjectPath &path, const QVariantMap &map)
+{
+}
+
+void OfonoModemManager::onModemRemoved(const QDBusObjectPath &path)
+{
+}
