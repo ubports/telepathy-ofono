a385798630d5794d435b6eca43d2f53eb668127f
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2013-12-04 12:31:39 -0200

    fix copyright

--- tests/approvertext.cpp
+++ tests/approvertext.cpp
@@ -1,22 +1,19 @@
-/*
- * Copyright (C) 2012 Canonical, Ltd.
+/**
+ * Copyright (C) 2013 Canonical, Ltd.
  *
- * Authors:
- *  Tiago Salem Herrmann <tiago.herrmann@canonical.com>
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3, as published by
+ * the Free Software Foundation.
  *
- * This file is part of telepathy-ofono.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+ * SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * telepathy-ofono is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3.
- *
- * telepathy-ofono is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Tiago Salem Herrmann <tiago.herrmann@canonical.com>
  */
 
 #include "approvertext.h"
